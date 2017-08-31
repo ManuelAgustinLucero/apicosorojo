b0f3ed857b74c6b8cd94c56f67212d26e6da7423
ManuelAgustinLucero <apattyeffect@gmail.com>
2017-08-31 17:55:32 -0300

    Company api 90%

--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -97,18 +97,29 @@ class CompanyController extends Controller
                     $em->persist($Company);
                     $em->flush();
 
-
+                    $company=array(
+                        "id"=>$Company->getId(),
+                        "name"=>$name,
+                        "title"=>$titledescription,
+                        "description"=>$description,
+                        "phone"=>$phone,
+                        "address"=>$address,
+                        "photoheader"=>$photoheader,
+                        "photoavatar"=>$photoavatar,
+                        "id_user"=>$user_id
+                    );
 
                    // $Company = $em->getRepository("AppBundle:Company")->findOneById($Company->getId());
 
+//
+//                    $Company = $em->getRepository("AppBundle:Company")->findOneBy(
+//                        array(
+//                            "id" =>$Company->getId(),
+//                        ),array('name' => 'ASC'),1,0);
 
-                    $Company = $em->getRepository("AppBundle:Company")->findOneBy(
-                        array(
-                            "user" => $user,
-                            "titledescription" => $titledescription,
-                        ));
 
                    $serializer = SerializerBuilder::create()->build();
+
                     $jsonObject = $serializer->serialize($Company, 'json');
 //
 //                    $normalizer = new ObjectNormalizer();
@@ -124,7 +135,7 @@ class CompanyController extends Controller
                     $data = array(
                         "status" => "success",
                         "code" => 200,
-                        "data" => $Company
+                        "data" => $company
                     );
                 } else {
                     $data = array(
@@ -157,14 +168,48 @@ class CompanyController extends Controller
      * @Route("/{id}", name="company_show")
      * @Method("GET")
      */
-    public function showAction(Company $company)
+    public function showAction(Request $request, $id = null)
     {
-        $deleteForm = $this->createDeleteForm($company);
+        $helpers = $this->get("app.helpers");
+        $em = $this->getDoctrine()->getManager();
 
-        return $this->render('company/show.html.twig', array(
-            'company' => $company,
-            'delete_form' => $deleteForm->createView(),
+        $Company = $em->getRepository("AppBundle:Company")->findOneBy(array(
+            "id" => $id
         ));
+
+//        $comments = $em->getRepository("AppBundle:User")->findBy(array(
+//           "id" => $company->getUser()->getId()
+//        ), array('id'=>'desc'));
+
+
+
+        $Company=array(
+            "id"=>$Company->getId(),
+            "name"=>$Company->getName(),
+            "title"=>$Company->getTitledescription(),
+            "description"=> $Company->getDescription(),
+            "phone"=> $Company->getPhone(),
+            "address"=>$Company->getAddress(),
+            "photoheader"=>$Company->getPhotoavatar(),
+            "photoavatar"=> $Company->getPhotoheader(),
+            "id_user"=>$Company->getUser()->getId()
+        );
+
+        if(count($Company) >= 1){
+            $data = array(
+                "status" => "success",
+                "code"	 => 200,
+                "data"	 => $Company
+            );
+        }else{
+            $data = array(
+                "status" => "error",
+                "code"	 => 400,
+                "msg"	 => "Dont exists this Company!!"
+            );
+        }
+
+        return $helpers->json($data);
     }
 
     /**
@@ -173,58 +218,154 @@ class CompanyController extends Controller
      * @Route("/{id}/edit", name="company_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, Company $company)
+    public function editAction(Request $request, $id = null)
     {
-        $deleteForm = $this->createDeleteForm($company);
-        $editForm = $this->createForm('AppBundle\Form\CompanyType', $company);
-        $editForm->handleRequest($request);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        $helpers = $this->get("app.helpers");
 
-            return $this->redirectToRoute('company_edit', array('id' => $company->getId()));
+        $hash = $request->get("authorization", null);
+        $authCheck = $helpers->authCheck($hash);
+
+        if ($authCheck == true) {
+            $identity = $helpers->authCheck($hash, true);
+
+            $json = $request->get("json", null);
+
+            if ($json != null) {
+                $params = json_decode($json);
+
+                $Company_id = $id;
+
+                $createdAt = new \Datetime('now');
+                $updatedAt = new \Datetime('now');
+
+                $user_id = ($identity->id != null) ? $identity->id : null;
+                $name = (isset($params->name)) ? $params->name : null;
+                $description = (isset($params->description)) ? $params->description : null;
+                $address = (isset($params->address)) ? $params->address : null;
+                $phone = (isset($params->phone)) ? $params->phone : null;
+                $titledescription = (isset($params->titledescription)) ? $params->titledescription : null;
+                $photoavatar = null;
+                $photoheader = null;
+
+                if ($name != null &&  $description != null &&  $user_id != null) {
+                    $em = $this->getDoctrine()->getManager();
+
+                    $Company = $em->getRepository("AppBundle:Company")->findOneBy(
+                        array(
+                            "id" => $Company_id
+                        ));
+
+                    if (isset($identity->id) && $identity->id == $Company->getUser()->getId()) {
+
+                        $Company->setName($name);
+                        $Company->setTitledescription($titledescription);
+                        $Company->setDescription($description);
+                        $Company->setPhone($phone);
+                        $Company->setAddress($address);
+                        $Company->setPhotoavatar($photoavatar);
+                        $Company->setPhotoheader($photoheader);
+
+                        $em->persist($Company);
+                        $em->flush();
+
+                        $data = array(
+                            "status" => "success",
+                            "code" => 200,
+                            "msg" => "Company updated success!!"
+                        );
+                    } else {
+                        $data = array(
+                            "status" => "success",
+                            "code" => 200,
+                            "msg" => "Company updated error, you not owner!!"
+                        );
+                    }
+                } else {
+                    $data = array(
+                        "status" => "error",
+                        "code" => 400,
+                        "msg" => "Company updated error"
+                    );
+                }
+            } else {
+                $data = array(
+                    "status" => "error",
+                    "code" => 400,
+                    "msg" => "Company not updated, params failed"
+                );
+            }
+        } else {
+            $data = array(
+                "status" => "error",
+                "code" => 400,
+                "msg" => "Authorization not valid"
+            );
         }
 
-        return $this->render('company/edit.html.twig', array(
-            'company' => $company,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+        return $helpers->json($data);
     }
 
     /**
      * Deletes a company entity.
      *
-     * @Route("/{id}", name="company_delete")
+     * @Route("/{id}/delete", name="company_delete")
      * @Method("DELETE")
      */
-    public function deleteAction(Request $request, Company $company)
+    public function deleteAction(Request $request, $id = null)
     {
-        $form = $this->createDeleteForm($company);
-        $form->handleRequest($request);
+        $helpers = $this->get("app.helpers");
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        $hash = $request->get("authorization", null);
+        $authCheck = $helpers->authCheck($hash);
+
+        if ($authCheck == true) {
+
+            $identity = $helpers->authCheck($hash, true);
+
+            $user_id = ($identity->id != null) ? $identity->id : null;
+
             $em = $this->getDoctrine()->getManager();
-            $em->remove($company);
-            $em->flush();
+
+            $Company = $em->getRepository("AppBundle:Company")->findOneBy(array(
+                "id" => $id
+            ));
+
+            if (is_object($Company) && $user_id != null) {
+                if (isset($identity->id) &&
+                    ($identity->id == $Company->getUser()->getId())) {
+
+                    $em->remove($Company);
+                    $em->flush();
+
+                    $data = array(
+                        "status" => "success",
+                        "code" => 200,
+                        "msg" => "Company deleted success"
+                    );
+                } else {
+                    $data = array(
+                        "status" => "error",
+                        "code" => 400,
+                        "msg" => "Company not deleted"
+                    );
+                }
+            } else {
+                $data = array(
+                    "status" => "error",
+                    "code" => 400,
+                    "msg" => "Company not deleted"
+                );
+            }
+        } else {
+            $data = array(
+                "status" => "error",
+                "code" => 400,
+                "msg" => "Authencation not valid"
+            );
         }
-
-        return $this->redirectToRoute('company_index');
+        return $helpers->json($data);
     }
 
-    /**
-     * Creates a form to delete a company entity.
-     *
-     * @param Company $company The company entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Company $company)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('company_delete', array('id' => $company->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
+
 }
